4589da28ff7d8441a848e2256a6e6736dc0710df
sumanth.jillepally <sumanth.jillepally@HYDPCM312077D.ad.infosys.com>
2017-04-28 12:10:03 +0530

    good bye greeting

--- GitHubHello/src/NewPackage/testclasss.java
+++ GitHubHello/src/NewPackage/testclasss.java
@@ -5,6 +5,7 @@ public class testclasss {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hello GitHub");
+System.out.println("bye git");
 	}
 
 }
