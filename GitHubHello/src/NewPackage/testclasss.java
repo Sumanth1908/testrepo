b609834db62ccbbc6b928708ceef2bd37f144d0a
sumanth.jillepally <sumanth.jillepally@HYDPCM312077D.ad.infosys.com>
2017-05-02 10:41:36 +0530

    removed sumanth

--- GitHubHello/src/NewPackage/testclasss.java
+++ GitHubHello/src/NewPackage/testclasss.java
@@ -5,7 +5,6 @@ public class testclasss {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hello GitHub");
-System.out.println("hello sumanth");
 System.out.println("bye git");
 	}
 
