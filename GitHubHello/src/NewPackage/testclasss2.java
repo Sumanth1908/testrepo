e93eeb74e9349291b6f4cf8d0d2c97582153a916
sumanth.jillepally <sumanth.jillepally@HYDPCM312077D.ad.infosys.com>
2017-05-02 16:38:54 +0530

    added bye to new class

--- GitHubHello/src/NewPackage/testclasss2.java
+++ GitHubHello/src/NewPackage/testclasss2.java
@@ -4,7 +4,10 @@ public class testclasss2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println("Bye git");
+System.out.println("Bye Sumanth");
+System.out.println("Bye Ram");
+System.out.println("Bye sam");
 	}
 
 }
