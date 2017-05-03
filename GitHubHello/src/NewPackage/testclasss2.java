f898f34e4113e83af914e6cd1eb7d6309bcb1dbe
sumanth.jillepally <sumanth.jillepally@HYDPCM312077D.ad.infosys.com>
2017-05-03 13:50:38 +0530

    only 2 bye world

--- GitHubHello/src/NewPackage/testclasss2.java
+++ GitHubHello/src/NewPackage/testclasss2.java
@@ -4,8 +4,8 @@ public class testclasss2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Bye git");
-System.out.println("Bye Sumanth");
+System.out.println("Bye world");
+System.out.println("Bye world");
 	}
 
 }
