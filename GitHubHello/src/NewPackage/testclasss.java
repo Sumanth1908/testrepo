c50c9f4090547db1932cdef03c82405e9125afd8
sumanth.jillepally <sumanth.jillepally@HYDPCM312077D.ad.infosys.com>
2017-05-04 11:27:53 +0530

    deleted sumath and bro hellos

--- GitHubHello/src/NewPackage/testclasss.java
+++ GitHubHello/src/NewPackage/testclasss.java
@@ -5,8 +5,6 @@ public class testclasss {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello World");
-System.out.println("Hello World");
-System.out.println("Hello Sumanth");
 System.out.println("Hello Bro");
 }
 }
