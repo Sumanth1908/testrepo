2483884d38e9229d9f57f599f9ef7a519da5f92b
Sumanth <jschary555@gmail.com>
2021-10-24 20:26:33 +0530

    removed

--- GitHubHello/src/NewPackage/testclasss.java
+++ GitHubHello/src/NewPackage/testclasss.java
@@ -4,8 +4,5 @@ public class testclasss {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello World");
-System.out.println("Hello World");
-System.out.println("Hello Sumanth");
 }
 }
